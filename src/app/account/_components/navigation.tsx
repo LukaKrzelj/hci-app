c8301ec402a4b979935634e773cc5b8de8fcb8b1
LukaKrzelj <lkrzel00@fesb.hr>
2024-11-21 22:05:18 +0100

    Created a structure as shown on sitemap

---
+++ src/app/account/_components/navigation.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}`;
+};
+
+// We hardcode pages here, but you could get this information from some external source (e.g. CMS, DB, config file, etc).
+const pages: Page[] = [
+  {
+    title: "Log In",
+    path: "/account/login",
+  },
+  {
+    title: "Loyalty Program",
+    path: "/account/loyalty",
+  },
+  {
+    title: "Order History",
+    path: "/account/order-history",
+  },
+  {
+    title: "Wishlist",
+    path: "/account/wishlist",
+  },
+];
+
+function processPage(page: Page, index: number, pathname: string) {
+  return (
+    <li key={index}>
+      <Link
+        href={page.path}
+        className={
+          pathname === page.path ? "font-extrabold text-slate-600" : ""
+        }
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export function Navigation() {
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <ul className="flex justify-center space-x-4 mt-8">
+      {pages.map((page, index) => processPage(page, index, pathname))}
+    </ul>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-6xl font-extrabold tracking-tight">Home page</h1>
+    </main>
   );
 }
